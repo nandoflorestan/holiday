cf57c062487f0a1d0b1742ff66c3cf219273e3af
Nando Florestan <nandoflorestan@gmail.com>
2013-11-10 19:50:50 -0200

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/nandoflorestan/holiday',
     name="holiday",
     author='Nando Florestan',
-    version='0.0.1dev',
+    version='0.0.1',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
